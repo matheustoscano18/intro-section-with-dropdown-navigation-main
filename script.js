1bda14674ad6d6c6e305d1e868a46c30c7ca3029
Matheus <matheustoscano2012@gmail.com>
2024-11-12 18:43:21 -0300

    landing page dropdown menu

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ const featuresButton = document.getElementById("features");
 const companyButton = document.getElementById("company");
 const dropdownFeatures = document.querySelector(".dropdown-content-features");
 const dropdownCompany = document.querySelector(".dropdown-content-company");
+const arrowIconFeatures = document.getElementById("arrow-icon-features");
+const arrowIconCompany = document.getElementById("arrow-icon-company");
 
-function toggleDropdown(dropdown) {
+const arrowDownIcon = "./images/icon-arrow-down.svg";
+const arrowUpIcon = "./images/icon-arrow-up.svg";
+
+function toggleDropdown(dropdown, arrowIcon) {
   if (dropdown.style.display === "block") {
     dropdown.style.display = "none";
+    arrowIcon.src = arrowDownIcon;
   } else {
     dropdown.style.display = "block";
+    arrowIcon.src = arrowUpIcon;
   }
 }
 
-featuresButton.addEventListener("click", () => toggleDropdown(dropdownFeatures));
-companyButton.addEventListener("click", () => toggleDropdown(dropdownCompany));
+featuresButton.addEventListener("click", () => toggleDropdown(dropdownFeatures, arrowIconFeatures));
+companyButton.addEventListener("click", () => toggleDropdown(dropdownCompany, arrowIconCompany));
 
 const openIcon = document.querySelector('.open-icon');
 const closeIcon = document.querySelector('.close-icon');
